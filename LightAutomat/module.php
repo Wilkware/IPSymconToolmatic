553965b11cae753a278a7e81a7111936f81fc3af
Wilkware <heiko.wilknitz@wilkware.de>
2016-12-20 11:06:18 +0100

    Implement Logic

--- LightAutomat/module.php
+++ LightAutomat/module.php
@@ -28,11 +28,30 @@
 					IPS_SetName($eid, "TriggerEvent");
 				}
 				IPS_SetEventTrigger($eid, 0, $this->ReadPropertyInteger("StateVariable"));
-				IPS_SetEventScript($eid, "TLA_Trigger(\$_IPS['TARGET']);");
+				IPS_SetEventScript($eid, "TLA_Activate(\$_IPS['TARGET'], \$_IPS['VALUE']);");
         IPS_SetEventActive($eid, true);
 			}
 		}
-	
+
+		/**
+		* This function will be available automatically after the module is imported with the module control.
+		* Using the custom prefix this function will be callable from PHP and JSON-RPC through:
+		*
+		* TLA_Activate($id, &value);
+		*
+		*/
+		public function Activate(boolean $value)
+		{
+      if ($value  == true) {
+        // Minutenberechnung = 1000ms * 1min(60s) * Duration
+        $this->SetTimerInterval("TriggerTimer", 1000 * 60 * $this->ReadPropertyInteger("Duration"));
+      }
+      else {
+        // Licht wurde schon manuell ausgeschaltet
+        $this->SetTimerInterval("TriggerTimer", 0);
+      }
+    }
+
 		/**
 		* This function will be available automatically after the module is imported with the module control.
 		* Using the custom prefix this function will be callable from PHP and JSON-RPC through:
@@ -42,9 +61,13 @@
 		*/
 		public function Trigger()
 		{
-			$this->SendDebug("TLA_Trigger", "Debug Message", 0);
-		}
-	
+      if (GetValue($this->ReadPropertyInteger("StateVariable"))==true) {
+        $pid = IPS_GetParent($this->ReadPropertyInteger("StateVariable"));
+        HM_WriteValueBoolean($pid, "STATE", false); //Gerät ausschalten
+        // WFC_PushNotification(xxxxx , 'Licht', '...wurde ausgeschalten!', '', 0);
+      }
+      $this->SetTimerInterval("TriggerTimer", 0);
+    }
 	}
 
 ?>
--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 Folgende Module beinhaltet das IP Symcon Toolmatic Repository:
 
-- __Lichtautomat__ ([Dokumentation](Lichtautomat))
-	Automatisches Ausschalten des Lichtes (oder Aktoren) nach einer bestimmten Zeit.
-
+- __Lichtautomat__ ([Dokumentation](Lightautomat))
+	Automatisches Ausschalten des Lichtes (oder Aktor) nach einer bestimmten Zeit.
