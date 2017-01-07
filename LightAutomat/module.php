82a277f78d39d006570af82cc229da899b524817
Wilkware <heiko.wilknitz@wilkware.de>
2017-01-07 23:07:22 +0100

    Update (v3.0)

--- LightAutomat/module.php
+++ LightAutomat/module.php
@@ -1,5 +1,9 @@
 <?
 
+define("IPS_BASE", 10000);
+define("IPS_VARIABLEMESSAGE", IPS_BASE + 600);
+define("VM_UPDATE", IPS_VARIABLEMESSAGE + 3);
+
 class LightAutomat extends IPSModule
 {
   
@@ -10,6 +14,8 @@ class LightAutomat extends IPSModule
     
     $this->RegisterPropertyInteger("StateVariable", 0);
     $this->RegisterPropertyInteger("Duration", 10);
+    $this->RegisterPropertyInteger("MotionVariable", 0);
+    $this->RegisterPropertyInteger("PermanentVariable", 0);
     $this->RegisterPropertyBoolean("ExecScript", false);
     $this->RegisterPropertyInteger("ScriptVariable", 0);
     $this->RegisterPropertyBoolean("OnlyScript", false);
@@ -19,15 +25,14 @@ class LightAutomat extends IPSModule
   public function ApplyChanges()
   {
     if($this->ReadPropertyInteger("StateVariable") != 0) {
-      $this->UnregisterMessage($this->ReadPropertyInteger("StateVariable"), 10603 /*VM_UPDATE*/);
+      $this->UnregisterMessage($this->ReadPropertyInteger("StateVariable"), VM_UPDATE);
     }
-
     //Never delete this line!
     parent::ApplyChanges();
     
     //Create our trigger
     if(IPS_VariableExists($this->ReadPropertyInteger("StateVariable"))) {
-      $this->RegisterMessage($this->ReadPropertyInteger("StateVariable"), 10603 /*VM_UPDATE*/);
+      $this->RegisterMessage($this->ReadPropertyInteger("StateVariable"), VM_UPDATE);
     }
   }
   
@@ -46,12 +51,17 @@ class LightAutomat extends IPSModule
 
     switch ($Message)
     {
-      case 10603 /*VM_UPDATE*/:
+      case VM_UPDATE:
         if ($SenderID != $this->ReadPropertyInteger("StateVariable")) {
           $this->SendDebug('Message:SenderID', $SenderID . " unbekannt!", 0);
           break;
         }
-        
+        // Dauerbetrieb, tue nix!
+        $pid = $this->ReadPropertyInteger("StateVariable");
+        if ($pid != 0 && GetValue($pid)) {
+          $this->SendDebug('MessageSink', "Dauerbetrieb ist angeschalten!", 0);
+          break;
+        }
         if ($Data[0] == true) {
           // Minutenberechnung = 1000ms * 1min(60s) * Duration
           $this->SetTimerInterval("TriggerTimer", 1000 * 60 * $this->ReadPropertyInteger("Duration"));
@@ -76,11 +86,17 @@ class LightAutomat extends IPSModule
     if (GetValue($this->ReadPropertyInteger("StateVariable")) == true) {
 
       if($this->ReadPropertyBoolean("OnlyScript") == false ) {
-        $pid = IPS_GetParent($this->ReadPropertyInteger("StateVariable"));
-      
-        HM_WriteValueBoolean($pid, "STATE", false); //Gerät ausschalten
-        $this->SendDebug('TLA_Trigger', "STATE von #" . $pid . " auf false geschalten!" , 0);
-        // WFC_PushNotification(xxxxx , 'Licht', '...wurde ausgeschalten!', '', 0);
+        $mid = IPS_GetParent($this->ReadPropertyInteger("MotionVariable"));
+        if($mid != 0 && GetValue($mid)) {
+          $this->SendDebug('TLA_Trigger', "Bewegungsmelder aktiv, also nochmal!" , 0);
+          return;
+        }
+        else {
+          $pid = IPS_GetParent($this->ReadPropertyInteger("StateVariable"));
+          HM_WriteValueBoolean($pid, "STATE", false); //Gerät ausschalten
+          $this->SendDebug('TLA_Trigger', "STATE von #" . $pid . " auf false geschalten!" , 0);
+          // WFC_PushNotification(xxxxx , 'Licht', '...wurde ausgeschalten!', '', 0);
+        }
       }    
       
       if($this->ReadPropertyBoolean("ExecScript") == true) {     
@@ -99,6 +115,19 @@ class LightAutomat extends IPSModule
       $this->SendDebug('TLA_Trigger', "STATE schon FALSE - Timer löschen!" , 0);
     }        
     $this->SetTimerInterval("TriggerTimer", 0);
+  }
+
+  /**
+  * This function will be available automatically after the module is imported with the module control.
+  * Using the custom prefix this function will be callable from PHP and JSON-RPC through:
+  *
+  * TLA_Duration($id, $duration);
+  *
+  */
+  public function Duration($duration)
+  {
+      IPS_SetProperty($this->InstanceID, "Duration", $duration);
+      IPS_ApplyChanges($this->InstanceID);
   }
 }
 
