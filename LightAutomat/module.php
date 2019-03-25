3e1741eb5e8a5c54696e95c26ee97e392af76a43
Heiko Wilknitz <heiko.wilknitz@wilkware.de>
2019-03-25 17:06:18 +0000

    Apply fixes from StyleCI

--- LightAutomat/module.php
+++ LightAutomat/module.php
@@ -1,158 +1,145 @@
-<?
+<?php
+
 // --- BASE MESSAGE
-if(!defined("IPS_BASE")) {
-  define("IPS_BASE", 10000);
+if (!defined('IPS_BASE')) {
+    define('IPS_BASE', 10000);
 }
 // --- VARIABLE MANAGER
-if (!defined("IPS_VARIABLEMESSAGE")) {
-  define("IPS_VARIABLEMESSAGE", IPS_BASE + 600);
-  define("VM_UPDATE", IPS_VARIABLEMESSAGE + 3);
+if (!defined('IPS_VARIABLEMESSAGE')) {
+    define('IPS_VARIABLEMESSAGE', IPS_BASE + 600);
+    define('VM_UPDATE', IPS_VARIABLEMESSAGE + 3);
 }
 
 // CLASS LightAutomat
 class LightAutomat extends IPSModule
 {
-  
-  public function Create()
-  {
-    //Never delete this line!
-    parent::Create();
-    
-    $this->RegisterPropertyInteger("StateVariable", 0);
-    $this->RegisterPropertyInteger("Duration", 10);
-    $this->RegisterPropertyInteger("MotionVariable", 0);
-    $this->RegisterPropertyInteger("PermanentVariable", 0);
-    $this->RegisterPropertyBoolean("ExecScript", false);
-    $this->RegisterPropertyInteger("ScriptVariable", 0);
-    $this->RegisterPropertyBoolean("OnlyBool", false);
-    $this->RegisterPropertyBoolean("OnlyScript", false);
-    $this->RegisterTimer("TriggerTimer",0,"TLA_Trigger(\$_IPS['TARGET']);");
-  }
-
-  public function ApplyChanges()
-  {
-    if($this->ReadPropertyInteger("StateVariable") != 0) {
-      $this->UnregisterMessage($this->ReadPropertyInteger("StateVariable"), VM_UPDATE);
-    }
-    
-    //Never delete this line!
-    parent::ApplyChanges();
-    
-    //Create our trigger
-    if(IPS_VariableExists($this->ReadPropertyInteger("StateVariable"))) {
-      $this->RegisterMessage($this->ReadPropertyInteger("StateVariable"), VM_UPDATE);
-    }
-  }
-  
-  /**
-   * Interne Funktion des SDK.
-   * data[0] = neuer Wert
-   * data[1] = wurde Wert geändert?
-   * data[2] = alter Wert
-   * data[3] = Timestamp
-   *
-   * @access public
-   */
-  public function MessageSink($timeStamp, $senderID, $message, $data)
-  {
-    // $this->SendDebug('MessageSink', 'SenderId: '. $senderID . ' Data: ' . print_r($data, true), 0);
-
-    switch ($message)
+    public function Create()
     {
+        //Never delete this line!
+        parent::Create();
+
+        $this->RegisterPropertyInteger('StateVariable', 0);
+        $this->RegisterPropertyInteger('Duration', 10);
+        $this->RegisterPropertyInteger('MotionVariable', 0);
+        $this->RegisterPropertyInteger('PermanentVariable', 0);
+        $this->RegisterPropertyBoolean('ExecScript', false);
+        $this->RegisterPropertyInteger('ScriptVariable', 0);
+        $this->RegisterPropertyBoolean('OnlyBool', false);
+        $this->RegisterPropertyBoolean('OnlyScript', false);
+        $this->RegisterTimer('TriggerTimer', 0, "TLA_Trigger(\$_IPS['TARGET']);");
+    }
+
+    public function ApplyChanges()
+    {
+        if ($this->ReadPropertyInteger('StateVariable') != 0) {
+            $this->UnregisterMessage($this->ReadPropertyInteger('StateVariable'), VM_UPDATE);
+        }
+
+        //Never delete this line!
+        parent::ApplyChanges();
+
+        //Create our trigger
+        if (IPS_VariableExists($this->ReadPropertyInteger('StateVariable'))) {
+            $this->RegisterMessage($this->ReadPropertyInteger('StateVariable'), VM_UPDATE);
+        }
+    }
+
+    /**
+     * Interne Funktion des SDK.
+     * data[0] = neuer Wert
+     * data[1] = wurde Wert geändert?
+     * data[2] = alter Wert
+     * data[3] = Timestamp.
+     */
+    public function MessageSink($timeStamp, $senderID, $message, $data)
+    {
+        // $this->SendDebug('MessageSink', 'SenderId: '. $senderID . ' Data: ' . print_r($data, true), 0);
+
+        switch ($message) {
       case VM_UPDATE:
         // Safty Check
-        if ($senderID != $this->ReadPropertyInteger("StateVariable")) {
-          $this->SendDebug('MessageSink', 'SenderID: ' . $senderID . " unbekannt!", 0);
-          break;
+        if ($senderID != $this->ReadPropertyInteger('StateVariable')) {
+            $this->SendDebug('MessageSink', 'SenderID: '.$senderID.' unbekannt!', 0);
+            break;
         }
         // Dauerbetrieb, tue nix!
-        $pid = $this->ReadPropertyInteger("PermanentVariable");
+        $pid = $this->ReadPropertyInteger('PermanentVariable');
         if ($pid != 0 && GetValue($pid)) {
-          $this->SendDebug('MessageSink', "Dauerbetrieb ist angeschalten!", 0);
-          break;
+            $this->SendDebug('MessageSink', 'Dauerbetrieb ist angeschalten!', 0);
+            break;
         }
-        
+
         if ($data[0] == true && $data[1] == true) { // OnChange auf TRUE, d.h. Angeschalten
-          $this->SendDebug('MessageSink', 'OnChange auf TRUE - Angeschalten', 0);
-          $this->SetTimerInterval("TriggerTimer", 1000 * 60 * $this->ReadPropertyInteger("Duration"));
-        }
-        else if ($data[0] == false && $data[1] == true) { // OnChange auf FALSE, d.h. Ausgeschalten
-          $this->SendDebug('MessageSink', 'OnChange auf FALSE - Ausgeschalten', 0);
-          $this->SetTimerInterval("TriggerTimer", 0);
-        }
-        else { // OnChange - keine Zustandsaenderung
-          $this->SendDebug('MessageSink', 'OnChange unveraendert - keine Zustandsaenderung', 0);
+            $this->SendDebug('MessageSink', 'OnChange auf TRUE - Angeschalten', 0);
+            $this->SetTimerInterval('TriggerTimer', 1000 * 60 * $this->ReadPropertyInteger('Duration'));
+        } elseif ($data[0] == false && $data[1] == true) { // OnChange auf FALSE, d.h. Ausgeschalten
+            $this->SendDebug('MessageSink', 'OnChange auf FALSE - Ausgeschalten', 0);
+            $this->SetTimerInterval('TriggerTimer', 0);
+        } else { // OnChange - keine Zustandsaenderung
+            $this->SendDebug('MessageSink', 'OnChange unveraendert - keine Zustandsaenderung', 0);
         }
       break;
     }
-  }   
-
-  /**
-  * This function will be available automatically after the module is imported with the module control.
-  * Using the custom prefix this function will be callable from PHP and JSON-RPC through:
-  *
-  * TLA_Trigger($id);
-  *
-  * @access public
-  */
-  public function Trigger()
-  {
-    $sv = $this->ReadPropertyInteger("StateVariable");
-    if (GetValueBoolean($sv) == true) {
-      if($this->ReadPropertyBoolean("OnlyScript") == false ) {
-        $mid = $this->ReadPropertyInteger("MotionVariable");
-        if($mid != 0 && GetValue($mid)) {
-          $this->SendDebug('TLA_Trigger', "Bewegungsmelder aktiv, also nochmal!" , 0);
-          return;
-        }
-        else {
-          if($this->ReadPropertyBoolean("OnlyBool") == true) {
-            SetValueBoolean($sv, false);
-          }
-          else {
-            $pid = IPS_GetParent($sv);          
-            $ret = @HM_WriteValueBoolean($pid, "STATE", false); //Gerät ausschalten
-            if($ret === false) {
-              $this->SendDebug('TLA_Trigger', 'Gerät konnte nicht ausgeschalten werden (UNREACH)!', 0);
-            }
-          }
-          $this->SendDebug('TLA_Trigger', "StateVariable (#" . $sv . ") auf false geschalten!" , 0);
-          // WFC_PushNotification(xxxxx , 'Licht', '...wurde ausgeschalten!', '', 0);
-        }
-      }    
-      // Script ausführen
-      if($this->ReadPropertyBoolean("ExecScript") == true) {     
-        if ($this->ReadPropertyInteger("ScriptVariable") <> 0) {
-          if (IPS_ScriptExists($this->ReadPropertyInteger("ScriptVariable"))) {
-              $rs = IPS_RunScript($this->ReadPropertyInteger("ScriptVariable"));
-              $this->SendDebug('Script Execute: Return Value', $rs, 0);
-          }
-        }
-        else {
-          $this->SendDebug("TLA_Trigger", "Script #" . $this->ReadPropertyInteger('ScriptVariable') . " existiert nicht!",0);
-        }
-      }
     }
-    else {
-      $this->SendDebug('TLA_Trigger', "STATE schon FALSE - Timer löschen!" , 0);
-    }        
-    $this->SetTimerInterval("TriggerTimer", 0);
-  }
 
-  /**
-  * This function will be available automatically after the module is imported with the module control.
-  * Using the custom prefix this function will be callable from PHP and JSON-RPC through:
-  *
-  * TLA_Duration($id, $duration);
-  *
-  * @access public
-  * @param  integer $duration Wartezeit einstellen.
-  */
-  public function Duration(int $duration)
-  {
-      IPS_SetProperty($this->InstanceID, "Duration", $duration);
-      IPS_ApplyChanges($this->InstanceID);
-  }
+    /**
+     * This function will be available automatically after the module is imported with the module control.
+     * Using the custom prefix this function will be callable from PHP and JSON-RPC through:.
+     *
+     * TLA_Trigger($id);
+     */
+    public function Trigger()
+    {
+        $sv = $this->ReadPropertyInteger('StateVariable');
+        if (GetValueBoolean($sv) == true) {
+            if ($this->ReadPropertyBoolean('OnlyScript') == false) {
+                $mid = $this->ReadPropertyInteger('MotionVariable');
+                if ($mid != 0 && GetValue($mid)) {
+                    $this->SendDebug('TLA_Trigger', 'Bewegungsmelder aktiv, also nochmal!', 0);
+
+                    return;
+                } else {
+                    if ($this->ReadPropertyBoolean('OnlyBool') == true) {
+                        SetValueBoolean($sv, false);
+                    } else {
+                        $pid = IPS_GetParent($sv);
+                        $ret = @HM_WriteValueBoolean($pid, 'STATE', false); //Gerät ausschalten
+                        if ($ret === false) {
+                            $this->SendDebug('TLA_Trigger', 'Gerät konnte nicht ausgeschalten werden (UNREACH)!', 0);
+                        }
+                    }
+                    $this->SendDebug('TLA_Trigger', 'StateVariable (#'.$sv.') auf false geschalten!', 0);
+                    // WFC_PushNotification(xxxxx , 'Licht', '...wurde ausgeschalten!', '', 0);
+                }
+            }
+            // Script ausführen
+            if ($this->ReadPropertyBoolean('ExecScript') == true) {
+                if ($this->ReadPropertyInteger('ScriptVariable') != 0) {
+                    if (IPS_ScriptExists($this->ReadPropertyInteger('ScriptVariable'))) {
+                        $rs = IPS_RunScript($this->ReadPropertyInteger('ScriptVariable'));
+                        $this->SendDebug('Script Execute: Return Value', $rs, 0);
+                    }
+                } else {
+                    $this->SendDebug('TLA_Trigger', 'Script #'.$this->ReadPropertyInteger('ScriptVariable').' existiert nicht!', 0);
+                }
+            }
+        } else {
+            $this->SendDebug('TLA_Trigger', 'STATE schon FALSE - Timer löschen!', 0);
+        }
+        $this->SetTimerInterval('TriggerTimer', 0);
+    }
+
+    /**
+     * This function will be available automatically after the module is imported with the module control.
+     * Using the custom prefix this function will be callable from PHP and JSON-RPC through:.
+     *
+     * TLA_Duration($id, $duration);
+     *
+     * @param int $duration Wartezeit einstellen.
+     */
+    public function Duration(int $duration)
+    {
+        IPS_SetProperty($this->InstanceID, 'Duration', $duration);
+        IPS_ApplyChanges($this->InstanceID);
+    }
 }
-
-?>
--- PresenceDetector/module.php
+++ PresenceDetector/module.php
@@ -1,145 +1,138 @@
-<?
+<?php
+
 // --- BASE MESSAGE
-if(!defined("IPS_BASE")) {
-  define("IPS_BASE", 10000);
+if (!defined('IPS_BASE')) {
+    define('IPS_BASE', 10000);
 }
 // --- VARIABLE MANAGER
-if (!defined("IPS_VARIABLEMESSAGE")) {
-  define("IPS_VARIABLEMESSAGE", IPS_BASE + 600);
-  define("VM_UPDATE", IPS_VARIABLEMESSAGE + 3);
+if (!defined('IPS_VARIABLEMESSAGE')) {
+    define('IPS_VARIABLEMESSAGE', IPS_BASE + 600);
+    define('VM_UPDATE', IPS_VARIABLEMESSAGE + 3);
 }
 
 // CLASS PresenceDetector
 class PresenceDetector extends IPSModule
 {
-  
-  public function Create()
-  {
-    //Never delete this line!
-    parent::Create();
-    
-    $this->RegisterPropertyInteger("MotionVariable", 0);
-    $this->RegisterPropertyInteger("BrightnessVariable", 0);
-    $this->RegisterPropertyInteger("ThresholdValue", 0);
-    $this->RegisterPropertyInteger("SwitchVariable", 0);
-    $this->RegisterPropertyInteger("ScriptVariable", 0);
-    $this->RegisterPropertyBoolean("OnlyBool", false);
-  }
-
-  public function ApplyChanges()
-  {
-    if($this->ReadPropertyInteger("MotionVariable") != 0) {
-      $this->UnregisterMessage($this->ReadPropertyInteger("MotionVariable"), VM_UPDATE);
-    }
-    
-    //Never delete this line!
-    parent::ApplyChanges();
-    
-    //Create our trigger
-    if(IPS_VariableExists($this->ReadPropertyInteger("MotionVariable"))) {
-      $this->RegisterMessage($this->ReadPropertyInteger("MotionVariable"), VM_UPDATE);
-    }
-  }
-  
-  /**
-   * Interne Funktion des SDK.
-   * data[0] = neuer Wert
-   * data[1] = wurde Wert geändert?
-   * data[2] = alter Wert
-   * data[3] = Timestamp
-   *
-   * @access public
-   */
-  public function MessageSink($timeStamp, $senderID, $message, $data)
-  {
-    // $this->SendDebug('MessageSink', 'SenderId: '. $senderID . 'Data: ' . print_r($data, true), 0);
-
-    switch ($message)
+    public function Create()
     {
+        //Never delete this line!
+        parent::Create();
+
+        $this->RegisterPropertyInteger('MotionVariable', 0);
+        $this->RegisterPropertyInteger('BrightnessVariable', 0);
+        $this->RegisterPropertyInteger('ThresholdValue', 0);
+        $this->RegisterPropertyInteger('SwitchVariable', 0);
+        $this->RegisterPropertyInteger('ScriptVariable', 0);
+        $this->RegisterPropertyBoolean('OnlyBool', false);
+    }
+
+    public function ApplyChanges()
+    {
+        if ($this->ReadPropertyInteger('MotionVariable') != 0) {
+            $this->UnregisterMessage($this->ReadPropertyInteger('MotionVariable'), VM_UPDATE);
+        }
+
+        //Never delete this line!
+        parent::ApplyChanges();
+
+        //Create our trigger
+        if (IPS_VariableExists($this->ReadPropertyInteger('MotionVariable'))) {
+            $this->RegisterMessage($this->ReadPropertyInteger('MotionVariable'), VM_UPDATE);
+        }
+    }
+
+    /**
+     * Interne Funktion des SDK.
+     * data[0] = neuer Wert
+     * data[1] = wurde Wert geändert?
+     * data[2] = alter Wert
+     * data[3] = Timestamp.
+     */
+    public function MessageSink($timeStamp, $senderID, $message, $data)
+    {
+        // $this->SendDebug('MessageSink', 'SenderId: '. $senderID . 'Data: ' . print_r($data, true), 0);
+
+        switch ($message) {
       case VM_UPDATE:
-        if ($senderID != $this->ReadPropertyInteger("MotionVariable")) {
-          // Safety Check
-          $this->SendDebug('MessageSink', $senderID . " unbekannt!", 0);
-          break;
+        if ($senderID != $this->ReadPropertyInteger('MotionVariable')) {
+            // Safety Check
+            $this->SendDebug('MessageSink', $senderID.' unbekannt!', 0);
+            break;
         }
         if ($data[0] == true && $data[1] == true) { // OnChange auf TRUE, d.h. Bewegung erkannt
-          $this->SendDebug('MessageSink', 'OnChange auf TRUE - Bewegung erkannt', 0);
-          $this->SwitchState();
+            $this->SendDebug('MessageSink', 'OnChange auf TRUE - Bewegung erkannt', 0);
+            $this->SwitchState();
+        } elseif ($data[0] == false && $data[1] == true) { // OnChange auf FALSE, d.h. keine Bewegung
+            $this->SendDebug('MessageSink', 'OnChange auf FALSE - keine Bewegung', 0);
+        } else { // OnChange auf FALSE, d.h. keine Bewegung
+            $this->SendDebug('MessageSink', 'OnChange unveraendert - keine Zustandsaenderung', 0);
         }
-        else if ($data[0] == false && $data[1] == true) { // OnChange auf FALSE, d.h. keine Bewegung
-          $this->SendDebug('MessageSink', 'OnChange auf FALSE - keine Bewegung', 0);
-        }
-        else { // OnChange auf FALSE, d.h. keine Bewegung
-          $this->SendDebug('MessageSink', 'OnChange unveraendert - keine Zustandsaenderung', 0);
-        }
-        
+
       break;
     }
-  }   
-
-  /**
-  * This function will be available automatically after the module is imported with the module control.
-  * Using the custom prefix this function will be callable from PHP and JSON-RPC through:
-  *
-  * TPD_SwitchState($id);
-  *
-  * @access public
-  */
-  public function SwitchState()
-  {
-    // Check Brightness
-    if ($this->ReadPropertyInteger("BrightnessVariable") <> 0) {
-      $bv = GetValue($this->ReadPropertyInteger("BrightnessVariable"));
-      $tv = $this->ReadPropertyInteger("ThresholdValue");
-      if ($tv != 0 && $bv > $tv) {
-        $this->SendDebug('SwitchState', 'Oberhalb Schwellwert: ' . $bv  . '(Schwellwert: ' . $tv . ')', 0);
-        return; // nix zu tun
-      }
-      $this->SendDebug('SwitchState', 'Immer oder unterhalb Schwellwert: ' . $bv  . ' (Schwellwert: ' . $tv . ')', 0);
     }
-    // Variable schalten          
-    if ($this->ReadPropertyInteger("SwitchVariable") <> 0) {
-      $sv = $this->ReadPropertyInteger("SwitchVariable");
-      if($this->ReadPropertyBoolean("OnlyBool") == true) {
-        SetValueBoolean($sv, true);
-      }
-      else {
-        $pid = IPS_GetParent($sv);          
-        $ret = @HM_WriteValueBoolean($pid, "STATE", true); //Gerät einschalten
-        if($ret === false) {
-          $this->SendDebug('SwitchState', 'Gerät konnte nicht eingeschalten werden (UNREACH)!', 0);
+
+    /**
+     * This function will be available automatically after the module is imported with the module control.
+     * Using the custom prefix this function will be callable from PHP and JSON-RPC through:.
+     *
+     * TPD_SwitchState($id);
+     */
+    public function SwitchState()
+    {
+        // Check Brightness
+        if ($this->ReadPropertyInteger('BrightnessVariable') != 0) {
+            $bv = GetValue($this->ReadPropertyInteger('BrightnessVariable'));
+            $tv = $this->ReadPropertyInteger('ThresholdValue');
+            if ($tv != 0 && $bv > $tv) {
+                $this->SendDebug('SwitchState', 'Oberhalb Schwellwert: '.$bv.'(Schwellwert: '.$tv.')', 0);
+
+                return; // nix zu tun
+            }
+            $this->SendDebug('SwitchState', 'Immer oder unterhalb Schwellwert: '.$bv.' (Schwellwert: '.$tv.')', 0);
         }
-      }
-      $this->SendDebug('SwitchState', "Variable (#" . $sv . ") auf true geschalten!" , 0);
+        // Variable schalten
+        if ($this->ReadPropertyInteger('SwitchVariable') != 0) {
+            $sv = $this->ReadPropertyInteger('SwitchVariable');
+            if ($this->ReadPropertyBoolean('OnlyBool') == true) {
+                SetValueBoolean($sv, true);
+            } else {
+                $pid = IPS_GetParent($sv);
+                $ret = @HM_WriteValueBoolean($pid, 'STATE', true); //Gerät einschalten
+                if ($ret === false) {
+                    $this->SendDebug('SwitchState', 'Gerät konnte nicht eingeschalten werden (UNREACH)!', 0);
+                }
+            }
+            $this->SendDebug('SwitchState', 'Variable (#'.$sv.') auf true geschalten!', 0);
+        }
+        // Script ausführen
+        if ($this->ReadPropertyInteger('ScriptVariable') != 0) {
+            if (IPS_ScriptExists($this->ReadPropertyInteger('ScriptVariable'))) {
+                $sr = IPS_RunScript($this->ReadPropertyInteger('ScriptVariable'));
+                $this->SendDebug('SwitchState', 'Script Return Value: '.$rs, 0);
+            }
+        }
     }
-    // Script ausführen
-    if ($this->ReadPropertyInteger("ScriptVariable") <> 0) {
-      if (IPS_ScriptExists($this->ReadPropertyInteger("ScriptVariable"))) {
-        $sr = IPS_RunScript($this->ReadPropertyInteger("ScriptVariable"));
-        $this->SendDebug('SwitchState', 'Script Return Value: '. $rs, 0);
-      }
-    }
-  }
 
-  /**
-  * This function will be available automatically after the module is imported with the module control.
-  * Using the custom prefix this function will be callable from PHP and JSON-RPC through:
-  *
-  * TPD_SetThreshold($id, $threshold);
-  *
-  * @access public
-  * @param  bool $threshold Helligkeitsschwellwert ab welchem geschalten werden soll.
-  * @return bool true if successful, otherwise false.
-  */
-  public function SetThreshold(int $threshold)
-  {
-    if ((($threshold % 5) == 0) && $threshold >= 0 && $threshold <= 50 || $threshold = 75 || $threshold = 100)  {
-      IPS_SetProperty($this->InstanceID, "ThresholdValue", $threshold);
-      IPS_ApplyChanges($this->InstanceID);
-      return true;
+    /**
+     * This function will be available automatically after the module is imported with the module control.
+     * Using the custom prefix this function will be callable from PHP and JSON-RPC through:.
+     *
+     * TPD_SetThreshold($id, $threshold);
+     *
+     * @param bool $threshold Helligkeitsschwellwert ab welchem geschalten werden soll.
+     *
+     * @return bool true if successful, otherwise false.
+     */
+    public function SetThreshold(int $threshold)
+    {
+        if ((($threshold % 5) == 0) && $threshold >= 0 && $threshold <= 50 || $threshold = 75 || $threshold = 100) {
+            IPS_SetProperty($this->InstanceID, 'ThresholdValue', $threshold);
+            IPS_ApplyChanges($this->InstanceID);
+
+            return true;
+        }
+
+        return false;
     }
-    return false;
-  }
 }
-
-?>
